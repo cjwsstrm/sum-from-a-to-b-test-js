e0de6e0bc72dfe49ac4973f20339e4863a83dbcc
cjwsstrm <cjwesstrom@gmail.com>
2018-01-23 11:53:10 -0800

    updated sum function to recursively sum from a to b, passes test

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// base case as if statement
+// function sum(fromN, toN) {
+//   if (fromN === toN) {
+//     return toN
+//   }
+//   return fromN + sum(fromN + 1, toN)
+// }
 
+
+// recursive case as if statement. 1 ms faster (7 vs 8)?
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN)
+  }
+  return toN
 }
+
+console.log(sum(3, 7))
+
 
 module.exports = sum;
